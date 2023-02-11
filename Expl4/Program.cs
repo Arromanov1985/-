16c03e4af03b4bc42637082a3cfb4917cb2612ae
Artem <aromanov.b@ya.ru>
2023-02-11 21:15:56 +0300

    Expl5

---
+++ Expl4/Program.cs
@@ -0,0 +1,6 @@
+﻿double numberA = new Random().Next(1, 10);
+console.WriteLine(numberA) 
+double numberB = new Random().Next(1, 10);
+console.WriteLine(numberB) 
+double result = numberA / numberB;
+Console.WriteLine(result);
