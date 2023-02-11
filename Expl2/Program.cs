dc4a6e2f2fe9f9606e7b14191ba614f99514ffbb
Artem <aromanov.b@ya.ru>
2023-02-11 15:48:10 +0300

    My commit message

---
+++ Expl2/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Ввелите свое имя: ");
+string username = Console.ReadLine();
+Console.Write("Привет, ");
+Console.Write(username);
+
